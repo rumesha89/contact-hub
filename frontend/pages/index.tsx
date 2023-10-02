fab584af29bd99f00355093d850e78dd1eece79d
Rumesha <ryohani89@gmail.com>
2023-10-02 11:36:39 -0700

    Add : - frontend test - deploy workflow

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,31 +7,32 @@ const items: Contact[] = [
     id: 1,
     name: "Rumesha",
     number: "7788488993",
-    email: "tharindu@gail.com",
+    email: "Rumesha@gail.com",
   },
   {
     id: 2,
     name: "Rumesha",
     number: "7788488993",
-    email: "tharindu@gail.com",
+    email: "Rumesha@gail.com",
   },
   {
     id: 3,
     name: "Rumesha",
     number: "7788488993",
-    email: "tharindu@gail.com",
+    email: "Rumesha@gail.com",
   },
   {
     id: 4,
     name: "Rumesha",
     number: "7788488993",
-    email: "tharindu@gail.com",
+    email: "Rumesha@gail.com",
   },
 ];
 
 export default function Home() {
   return (
     <Container>
+      <h2>Contact Hub</h2>
       <ContactList items={items} />
     </Container>
   );
