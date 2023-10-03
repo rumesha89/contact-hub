d92bfbe696b37bd38e9744e387230fe54ec0812a
Rumesha Ranathunga <ryohani89@gmail.com>
2023-10-03 01:04:58 -0700

    Changing Basic Auth to JWT

--- backend/src/main/java/com/vrealcompany/contacthub/ContactHubApplication.java
+++ backend/src/main/java/com/vrealcompany/contacthub/ContactHubApplication.java
@@ -1,8 +1,14 @@
 package com.vrealcompany.contacthub;
 
+import com.vrealcompany.contacthub.enums.Role;
+import com.vrealcompany.contacthub.model.User;
+import com.vrealcompany.contacthub.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +16,16 @@ public class ContactHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactHubApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(UserRepository userRepository) {
+		return (args) -> {
+			User user = User.builder().firstName("Rumesha").lastName("Ranathunga")
+					.email("rumesha@mail.com").password(new BCryptPasswordEncoder().encode("password"))
+					.role(Role.USER).build();
+			userRepository.save(user);
+		};
 	}
 
 }
--- backend/src/main/java/com/vrealcompany/contacthub/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/vrealcompany/contacthub/configuration/SecurityConfiguration.java
@@ -1,17 +1,24 @@
 package com.vrealcompany.contacthub.configuration;
 
+import com.vrealcompany.contacthub.service.UserService;
+import com.vrealcompany.contacthub.util.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -19,34 +26,43 @@ public class SecurityConfiguration {
 
     public static final String[] ENDPOINTS_WHITELIST = {};
     public static final String RESTRICTED_URL_PATH = "/api/**";
-    public static final String USERNAME = "admin@vrcompany.com";
-    public static final String PASSWORD = "password";
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private UserService userService;
 
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails user = User.builder()
-                .username(USERNAME)
-                .password(encoder().encode(PASSWORD))
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
                 .securityMatcher(RESTRICTED_URL_PATH)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.PUT).authenticated()
                         .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService.userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
