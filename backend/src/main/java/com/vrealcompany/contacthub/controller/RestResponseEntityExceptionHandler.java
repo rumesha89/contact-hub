b21fac1b790c6ca98d648e7b67321ecd9b6ddd63
Rumesha Ranathunga <ryohani89@gmail.com>
2023-10-02 18:46:12 -0700

    Sonarcube fixes

--- backend/src/main/java/com/vrealcompany/contacthub/controller/RestResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/vrealcompany/contacthub/controller/RestResponseEntityExceptionHandler.java
@@ -23,7 +23,7 @@ public class RestResponseEntityExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
